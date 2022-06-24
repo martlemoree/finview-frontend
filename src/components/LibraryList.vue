d1ad9180d84abce44a6a504737a4de1bf4b91d25
martlemoree <71133462+martlemoree@users.noreply.github.com>
2022-06-25 01:01:18 +0200

    ui changes

--- src/components/LibraryList.vue
+++ src/components/LibraryList.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="row row-cols-1 row-cols-md-2 g-4">
-    <div class="col" v-for="library in libraries" :key="library.id">
-      <div class="col">
-        <div class="card">
-          <img :src="getImage(library)" class="card-img-top" :alt="library.libraryName">
-          <div class="card-body">
-            <h5 class="card-title">{{ library.libraryName }}</h5>
-            <p class="card-text">
-              {{ library.libraryName }} ist eine {{ library.programmingLanguage }} Bibliothek.
-              Die neueste Version ist die {{ library.latestVersion }} und es wird für {{ library.useField }} benutzt.
-            </p>
+  <div class="col" v-for="library in libraries" :key="library.id">
+    <div class="accordion" id="accordionPanelsStayOpenExample">
+      <div class="accordion-item">
+        <h2 class="accordion-header" id="panelsStayOpen-headingOne">
+          <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
+            <img :src="getImage(library)" :alt="library.libraryName" height="36" width="54"> {{ library.libraryName }}
+          </button>
+        </h2>
+        <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
+          <div class="accordion-body">
+            <strong>The {{ library.libraryName }} library is written in {{ library.programmingLanguage }}.</strong> At the moment the latest version is <code>{{ library.latestVersion }}</code>. The library is used in {{ library.useField }}.
           </div>
         </div>
       </div>
@@ -28,10 +28,48 @@ export default {
   },
   methods: {
     getImage (library) {
-      if (library.programmingLanguage === 'PYTHON') {
-        return require('../assets/python.png')
-      } else if (library.programmingLanguage === 'JAVASCRIPT') {
+      if (library.programmingLanguage === 'JavaScript') {
         return require('../assets/javascript.png')
+      } else if (library.programmingLanguage === 'Python') {
+        return require('../assets/python.png')
+      } else if (library.programmingLanguage === 'Java') {
+        return require('../assets/java.png')
+      } else if (library.programmingLanguage === 'PHP') {
+        return require('../assets/php.png')
+      } else if (library.programmingLanguage === 'CSS') {
+        return require('../assets/css.png')
+      } else if (library.programmingLanguage === 'C#') {
+        return require('../assets/csharp.png')
+      } else if (library.programmingLanguage === 'C++') {
+        return require('../assets/cplusplus.png')
+      } else if (library.programmingLanguage === 'Typescript') {
+        return require('../assets/typescript.png')
+      } else if (library.programmingLanguage === 'Ruby') {
+        return require('../assets/ruby.png')
+      } else if (library.programmingLanguage === 'C') {
+        return require('../assets/c.png')
+      } else if (library.programmingLanguage === 'Swift') {
+        return require('../assets/swift.png')
+      } else if (library.programmingLanguage === 'R') {
+        return require('../assets/r.png')
+      } else if (library.programmingLanguage === 'Objective-C') {
+        return require('../assets/objectivec.png')
+      } else if (library.programmingLanguage === 'Shell') {
+        return require('../assets/shell.png')
+      } else if (library.programmingLanguage === 'Scala') {
+        return require('../assets/scala.png')
+      } else if (library.programmingLanguage === 'Go') {
+        return require('../assets/go.png')
+      } else if (library.programmingLanguage === 'PowerShell') {
+        return require('../assets/powershell.png')
+      } else if (library.programmingLanguage === 'Kotlin') {
+        return require('../assets/kotlin.png')
+      } else if (library.programmingLanguage === 'Rust') {
+        return require('../assets/rust.png')
+      } else if (library.programmingLanguage === 'Dart') {
+        return require('../assets/dart.png')
+      } else if (library.programmingLanguage === 'other') {
+        return require('../assets/other.png')
       }
     }
   }
